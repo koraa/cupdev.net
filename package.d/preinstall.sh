2d87aed252938cccdb44f27631bef374e76c48fa
mappleconfusers <mapc@cupdev.net>
2013-07-11 00:35:39 +0200

    Clone highlightjs for it's themes

--- package.d/preinstall.sh
+++ package.d/preinstall.sh
@@ -5,3 +5,20 @@ dir="`dirname "$0"`"
 ################### Dirs #############################
 
 mkdir -p "$libs" "$meta"
+
+#################### GIT Install ####################
+# These are installed here in order to do shallow
+# clones instad of full ones, thereby accellerating
+# thereby install process enormously
+(
+  cd "$meta"
+  git clone --depth 1 "https://github.com/twitter/bootstrap.git" twitter-bootstrap
+  git clone --depth 1 "https://github.com/jnordberg/wintersmith-browserify.git" wintersmith-browserify
+  git clone --depth 1 "https://github.com/smebberson/wintersmith-jade.git" wintersmith-jade
+  git clone --depth 1 "https://github.com/isagalaev/highlight.js.git" highlight.js
+
+  cd "$pkgrt"
+  npm install "$meta/twitter-bootstrap"
+  npm install "$meta/wintersmith-browserify"
+  npm install "$meta/wintersmith-jade"
+)
