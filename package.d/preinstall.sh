ca4d1ea14140c230edf7f3333324eba3e0bba0e3
mappleconfusers <mapc@cupdev.net>
2013-07-11 00:19:13 +0200

    Rm highlightjs initcode

--- package.d/preinstall.sh
+++ package.d/preinstall.sh
@@ -5,28 +5,3 @@ dir="`dirname "$0"`"
 ################### Dirs #############################
 
 mkdir -p "$libs" "$meta"
-
-################### highlight.js #####################
-
-(
-  hljs="$meta/highlight.js"
-  hljs_repo="https://github.com/isagalaev/highlight.js.git"
-
-  # Update
-  if test -d "$hljs"; then
-      cd "$hljs"
-      git pull origin master
-  else
-    git clone "$hljs_repo" "$hljs" --depth 1
-  fi
-
-  # Compile
-  cd "$hljs"
-  python3 tools/build.py -tnode apache bash coffeescript cpp css diff http javascript java python xml tex
-
-  # Install
-  cd "$pkgrt"
-  npm install "$hljs/build"
-)
-
-
