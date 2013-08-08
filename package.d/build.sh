56276dbc0ca53ff7cca1efd0743e38b03082f0d9
Karolin Varner <karo@cupdev.net>
2013-08-08 23:32:45 +0200

    Externalize all npm commands

---
+++ package.d/build.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+#
+# Test stript.
+#
+# This basically starts the 
+
+###############################
+# INIT
+
+dir="`dirname "$0"`"
+. "$dir"/common.sh
+
+###############################
+# HELPER
+
+wintersmith build
