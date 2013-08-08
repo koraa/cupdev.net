61fc8ddf1adb72b32a48eb4d331411bc6c725ac6
Karolin Varner <karo@cupdev.net>
2013-08-08 23:27:31 +0200

    build,bash function: Set var by reference

--- package.d/common.sh
+++ package.d/common.sh
@@ -9,6 +9,14 @@ meta="$libs/__meta"
 
 ################### Functions ########################
 
+#
+# Set by reference
+# Invoke Rset [varname] [value]
+Rset() {
+  echo >&2 "rset ($0 | $1, $2, $3)"
+  eval "$1='$2'"
+}
+
 mkcd() {
   mkdir -p "$1"
     cd "$1"
