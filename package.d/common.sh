2c81dcb35358483542f92d6f145f93c6f1c87caa
Karolin Varner <karo@cupdev.net>
2013-08-08 23:29:06 +0200

    Convenience for setting colon delimited env vars

--- package.d/common.sh
+++ package.d/common.sh
@@ -17,6 +17,42 @@ Rset() {
   eval "$1='$2'"
 }
 
+#
+# Extend the given path and export it
+# while implementing linewise-tokens
+# Stripping can  be suppressed with colons
+exx() {
+  echo >&2 "rxx ($0 | $1, $2, $3)"
+  local ref OL NU filter
+ 
+  filter='
+   # ADDR      # COMMAND    # COMMENT
+   ###################################################
+                            #
+                s/^\s*//    # STRIP start of line
+                s/\s*$//    # STRIP end of line
+                            #
+                s/:/\n/     # colon to newline
+                            #
+    2,$         s/^/:/      # Prepend colon to all lines but first and last
+                            # 
+    /[^:\s*]/   p           # Filter empty lines
+  '
+
+  ref="$1"
+  OL="${!ref}"
+
+  # Apply transformations to STDIN
+  NU="$(sed -n "$filter" | tr -d '\n')"
+
+  # Write to var
+  Rset "$ref" "${NU}${OL}"
+  export "$ref"
+  
+  # DEBUG
+  echo >&2 "Extend env: ${ref}=${!ref}"
+}
+
 mkcd() {
   mkdir -p "$1"
     cd "$1"
