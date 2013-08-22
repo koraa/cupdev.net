c32fb0af3e13ae63f5997ac1bd778f7551b15d5d
Karolin Varner <karo@cupdev.net>
2013-08-23 01:49:59 +0200

    Add console target

---
+++ package.d/console.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+#
+# Test stript.
+#
+# This basically starts the 
+
+###############################
+# INIT
+
+dir="`dirname "$0"`"
+. "$dir"/common.sh
+
+################################
+## MAIN
+
+# TODO: Add default requires?
+coffee
