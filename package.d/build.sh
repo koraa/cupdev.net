bfd3ed5abdcfebbce1c8a8a389e64a571b1192f9
Karolin Varner <karo@cupdev.net>
2015-03-17 23:25:08 +0100

    The wintersmith build is now verbose
    also make the build script smaller

--- package.d/build.sh
+++ package.d/build.sh
@@ -1,16 +1,6 @@
 #! /bin/sh
-#
-# Test stript.
-#
-# This basically starts the 
-
-###############################
-# INIT
 
 dir="`dirname "$0"`"
 . "$dir"/common.sh
 
-###############################
-# HELPER
-
-node_modules/wintersmith/bin/wintersmith build
+node_modules/.bin/wintersmith build -v
